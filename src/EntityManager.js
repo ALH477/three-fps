305f0fedbd165680f80dedaa24dffd031bf32947
Asher <146795551+ALH477@users.noreply.github.com>
2025-07-16 16:14:18 -0700

    Update EntityManager.js

--- src/EntityManager.js
+++ src/EntityManager.js
@@ -1,15 +1,15 @@
-export default class EntityManager{
-    constructor(){
+export default class EntityManager {
+    constructor() {
         this.ids = 0;
         this.entities = [];
     }
 
-    Get(name){
-        return this.entities.find(el=>el.Name===name);
+    Get(name) {
+        return this.entities.find(el => el.Name === name);
     }
 
-    Add(entity){
-        if(!entity.Name){
+    Add(entity) {
+        if (!entity.Name) {
             entity.SetName(this.ids);
         }
         entity.id = this.ids;
@@ -18,21 +18,21 @@ export default class EntityManager{
         this.entities.push(entity);
     }
 
-    EndSetup(){
-        for(const ent of this.entities){
-            for(const key in ent.components){
+    EndSetup() {
+        for (const ent of this.entities) {
+            for (const key in ent.components) {
                 ent.components[key].Initialize();
             }
         }
     }
 
-    PhysicsUpdate(world, timeStep){
+    PhysicsUpdate(world, timeStep) {
         for (const entity of this.entities) {
             entity.PhysicsUpdate(world, timeStep);
         }
     }
 
-    Update(timeElapsed){
+    Update(timeElapsed) {
         for (const entity of this.entities) {
             entity.Update(timeElapsed);
         }
