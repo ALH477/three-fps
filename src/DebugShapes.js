64a562c9aed501e255bcf0c1518e6381d2dd8e8e
Asher <146795551+ALH477@users.noreply.github.com>
2025-07-16 16:15:19 -0700

    Update DebugShapes.js

--- src/DebugShapes.js
+++ src/DebugShapes.js
@@ -1,25 +1,24 @@
 import * as THREE from 'three'
 
-export default class DebugShapes{
-    constructor(scene){
+export default class DebugShapes {
+    constructor(scene) {
         this.scene = scene;
         this.meshes = [];
-        this.pointGeom = new THREE.SphereGeometry( 0.3, 8, 8 );
+        this.pointGeom = new THREE.SphereGeometry(0.3, 8, 8);
     }
 
-    AddPoint(position, color){
-        const material = new THREE.MeshBasicMaterial( {color, wireframe: true} );
-        const sphere = new THREE.Mesh( this.pointGeom, material );
+    AddPoint(position, color) {
+        const material = new THREE.MeshBasicMaterial({ color, wireframe: true });
+        const sphere = new THREE.Mesh(this.pointGeom, material);
         sphere.position.copy(position);
-        this.scene.add( sphere );
+        this.scene.add(sphere);
         this.meshes.push(sphere);
     }
     
-    Clear(){
-        for(const mesh of this.meshes){
+    Clear() {
+        for (const mesh of this.meshes) {
             this.scene.remove(mesh);
         }
-    
         this.meshes.length = 0;
     }
 }
